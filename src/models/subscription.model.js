67b694bd07ff791c6d236bcd2869eed3d166fd9f
xAvi2050 <cheetahmindset7@gmail.com>
2025-08-27 13:10:37 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true });
+
+// Prevent duplicate subscriptions
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true });
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
