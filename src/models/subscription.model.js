22990928ac2a300633c1cf0d605749f7c1bb70be
xAvi2050 <cheetahmindset7@gmail.com>
2025-09-03 12:41:23 +0530

    add getUserChannelProfile to fetch channel profile with subscription stats

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
 
 const subscriptionSchema = new Schema({
     subscriber: {
@@ -15,5 +16,13 @@ const subscriptionSchema = new Schema({
 
 // Prevent duplicate subscriptions
 subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true });
+
+// Prevent self-subscriptions
+subscriptionSchema.pre("save", async function (next) {
+    if (this.subscriber.toString() === this.channel.toString()) {
+        throw new ApiError("You cannot subscribe to yourself");
+    }
+    next();
+});
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
